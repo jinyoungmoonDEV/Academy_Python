9371ce686a0f2dbc5c9c5d7061e658cea13b563e
jinyoungmoonDEV <97270590+jinyoungmoonDEV@users.noreply.github.com>
2022-03-24 17:55:39 +0900

    Add files via upload

---
+++ 2022.03.24/LottoModule.py
@@ -0,0 +1,4 @@
+import random
+
+def getNumber() :
+    return random.randrange(1,46)
---
+++ 2022.03.24/Module1.py
@@ -0,0 +1,8 @@
+def func1() :
+    print("module1.py func1()")
+def func2() :
+    print("module1.py func2()")
+def func3() :
+    print("module1.py func3()")
+    
+PI = 3.14
